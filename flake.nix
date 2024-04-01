f8a260b143200b3481805d9e685b16442f59bb10
David Knaack <davidkna@users.noreply.github.com>
2024-04-01 15:16:22 +0200

    refactor: use nix

---
+++ flake.nix
@@ -0,0 +1,68 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      crane,
+      flake-utils,
+      nixpkgs,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = (import nixpkgs) { inherit system; };
+        inherit (pkgs) lib;
+
+        craneLib = crane.lib.${system};
+        src = craneLib.cleanCargoSource (craneLib.path ./.);
+
+        commonArgs = craneLib.crateNameFromCargoToml { cargoToml = ./lcat/Cargo.toml; } //{
+          inherit src;
+          strictDeps = true;
+
+          buildInputs = lib.optionals pkgs.stdenv.isDarwin (with pkgs; [
+            iconv
+            darwin.apple_sdk.frameworks.Security
+          ]);
+        } ;
+
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+        lcat = craneLib.buildPackage (commonArgs // { inherit cargoArtifacts; });
+      in
+      {
+        formatter = nixpkgs.legacyPackages.${system}.nixfmt-rfc-style;
+
+        checks = {
+          inherit lcat;
+          lcat-clippy = craneLib.cargoClippy (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+              cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+            }
+          );
+
+          lcat-fmt = craneLib.cargoFmt commonArgs;
+          lcat-deny = craneLib.cargoDeny commonArgs;
+        };
+
+        packages.default = lcat;
+
+        apps.default = flake-utils.lib.mkApp { drv = lcat; };
+
+        devShells.default = craneLib.devShell {
+          # Inherit inputs from checks.
+          checks = self.checks.${system};
+        };
+      }
+    );
+}
