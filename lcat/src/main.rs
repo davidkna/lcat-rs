66eef55d9bf7df4f184799388381322dd6a92550
Nor Khasyatillah <56464409+mazznoer@users.noreply.github.com>
2024-09-27 04:01:02 +0700

    feat(lcat): colorize `--help` (#152)

--- lcat/src/main.rs
+++ lcat/src/main.rs
@@ -6,17 +6,21 @@ use std::{
     path::PathBuf,
 };
 
-use clap::Parser;
+use clap::{CommandFactory, Parser};
 use lcat::{Rainbow, RainbowCmd};
 
 #[derive(Parser)]
-#[clap(name = "lcat", about = "Terminal rainbows.")]
+#[clap(name = "lcat", about = "Terminal rainbows.", disable_help_flag = true)]
 pub struct Cmdline {
     #[clap(name = "File", default_value = "-")]
     files: Vec<PathBuf>,
 
     #[clap(flatten)]
     rainbow: RainbowCmd,
+
+    /// Print help
+    #[clap(short = 'h', long)]
+    help: bool,
 }
 
 fn main() -> Result<(), io::Error> {
@@ -24,6 +28,14 @@ fn main() -> Result<(), io::Error> {
 
     let mut rainbow: Rainbow = opt.rainbow.into();
     let mut stdout = io::stdout().lock();
+
+    if opt.help {
+        rainbow.colorize_str(
+            &Cmdline::command().render_help().ansi().to_string(),
+            &mut stdout,
+        )?;
+        return Ok(());
+    }
 
     for path in opt.files {
         if path == PathBuf::from("-") {
